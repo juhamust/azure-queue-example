e42a383a92156be9de1795b84bfc67376b129589
Juha Mustonen <juha.mustonen@nordcloud.com>
2019-11-25 23:04:28 +0200

    Start using hooks

--- app/src/App.tsx
+++ app/src/App.tsx
@@ -1,9 +1,32 @@
-import React from "react";
+import React, { useState, useEffect, useCallback } from "react";
+
+interface DataEntry {
+  id: string;
+  value: string;
+}
 
 const App: React.FC = () => {
+  const [data, setData] = useState<DataEntry[] | null>([]);
+
+  const loadData = useCallback(() => {
+    // TODO: Implement
+    setData([{ id: "abc", value: "dang" }]);
+  }, []);
+
+  useEffect(() => {
+    loadData();
+  });
+
   return (
     <div className="App">
       <header className="App-header">Works!</header>
+      <ul>
+        {(data || []).map(entry => (
+          <li>
+            {entry.id}: {entry.value}
+          </li>
+        ))}
+      </ul>
     </div>
   );
 };
