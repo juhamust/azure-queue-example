fa6308c0fec5e695a084c6c0c88f75f8164815f9
Juha Mustonen <juha.mustonen@nordcloud.com>
2019-11-29 23:00:20 +0200

    Using chart instead of prog bar

--- app/src/App.tsx
+++ app/src/App.tsx
@@ -1,110 +1,48 @@
-import React, { useState, useEffect, useCallback } from 'react'
-import axios from 'axios'
+import React from 'react'
 import Container from '@material-ui/core/Container'
-import { makeStyles } from '@material-ui/core/styles'
 import {
-  LinearProgress,
   List,
   ListItem,
   ListItemText,
   Button,
   Typography,
 } from '@material-ui/core'
-
-interface DataEntry {
-  text: string
-  meta: any
-}
-
-const useStyles = makeStyles(theme => ({
-  progressBar: {
-    height: '20px',
-    margin: '2rem 0',
-  },
-}))
+import TaskChart from './TaskChart'
+import { useTasker } from './hooks'
 
 const App: React.FC = () => {
   const apiUrl = process.env.REACT_APP_API_URL || 'notset.com'
-  const classes = useStyles()
-  const [data, setData] = useState<DataEntry[] | null>([])
-  const [completed, setCompleted] = React.useState(0)
-
-  const progress = React.useRef(() => {})
-  React.useEffect(() => {
-    progress.current = () => {
-      if (completed > 100) {
-        setCompleted(0)
-      } else {
-        const diff = Math.random() * 10
-        setCompleted(completed + diff)
-      }
-    }
-  })
-
-  React.useEffect(() => {
-    function tick() {
-      progress.current()
-    }
-    const timer = setInterval(tick, 500)
-
-    return () => {
-      clearInterval(timer)
-    }
-  }, [])
-
-  const loadData = useCallback(
-    async url => {
-      const response = await axios({
-        url,
-        method: 'GET',
-      })
-      setData(response.data)
-    },
-    [setData]
-  )
-
-  const addTasks = useCallback(
-    async url => {
-      await axios({
-        url,
-        method: 'POST',
-      })
-      loadData(apiUrl)
-    },
-    [loadData, apiUrl]
-  )
-
-  useEffect(() => {
-    loadData(apiUrl)
-    setInterval(() => {
-      loadData(apiUrl)
-    }, 10000)
-  }, [apiUrl, loadData])
+  const tasker = useTasker(apiUrl)
 
   return (
     <Container>
-      <LinearProgress
-        className={classes.progressBar}
-        variant="buffer"
-        value={data && data.length > 0 ? completed : 0}
-        valueBuffer={100}
+      <TaskChart
+        series={[
+          {
+            name: 'test',
+            type: 'line',
+            data: tasker.seriesData,
+          },
+        ]}
       />
       <Button
         variant="contained"
         color="primary"
-        onClick={() => addTasks(apiUrl)}
+        onClick={() => tasker.addTasks(apiUrl)}
       >
         Add tasks
       </Button>
       <Typography>
         Currently{' '}
-        {data && data.length > 0 ? `${data.length} tasks` : 'no tasks'}
+        {tasker.tasks && tasker.tasks.length > 0
+          ? `${tasker.tasks.length} tasks`
+          : 'no tasks'}
       </Typography>
       <List dense={true}>
-        {(data || []).map((entry, index) => {
+        {(tasker.tasks || []).map((task, index) => {
           return (
             <ListItem key={index}>
-              <ListItemText primary={entry.text} />
+              <ListItemText primary={task} />
             </ListItem>
           )
         })}
